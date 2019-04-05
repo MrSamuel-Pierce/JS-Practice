f102379980384d89ef9a580834146936175d3550
TSN Student <student@ulem.org>
2019-04-05 16:04:40 -0400

    javascript practice

---
+++ Assignment 3/Main.js
@@ -0,0 +1,87 @@
+// Question 1
+// Write the function printInt(n)
+// taking one parameter n and print all natural numbers
+// from 1 to n in console.
+function printInt(n) {
+    // Using a loop print from 1 to n 
+    // start: 1
+    var i = 1 
+    // stop: n
+    while(i <= n){
+        console.log(i)
+    // step: +1
+        i++;
+    }
+}
+printInt(5);
+
+// Question 2
+
+
+
+
+
+
+// Question 3
+
+// return the string 'string' if x is a string;
+// and return 'boolean' if x is a boolean.
+// Otherwise returns -1.
+// function checkpoint(x){
+    // if x is a boolean, number or string
+    // then we return its type
+    // otherwise we return -1
+
+if(){
+
+} else {
+
+}
+
+// console.log(checkInput(null))
+
+
+// Question 4
+
+// Write the function simpleEvenAdding(num)
+//taking a number and add up all the numbers from 1 to num.
+function simpleEvenAdding(num){
+    // store the answer
+    var solution=...;
+    // loop over numbers from 1 to num
+    for(...;...;...){
+        // check if current index is even or odd
+        if(...) {
+
+        }
+    }
+
+    return solution;
+}
+
+// Question 5
+// Write the function letterCapitolize(str)
+// taking a string and capitolize the first letter
+// of each word. 
+// The given words will be separated 
+// by only one space
+function letterCapitolize(Str){
+    // separate the str into array of words
+    // using split
+
+    // loop over the array of words
+
+    // capitolize the first character of each word
+}
+
+// Question 6
+// "hello".split("");
+// (5) ["h", "e", "l", "l", "o"]
+// function examples include:
+// join
+// +
+// .toString
+
+// Question 7
+// Write the function findDiff(arr) 
+// taking an array of number
---
+++ Assignment 3/Martin.js
@@ -0,0 +1,36 @@
+console.log("Wake Up Computer!");
+alert("What do you know about Katherine Johnson?");
+
+function printInt(n) {
+
+// Using a loop print from 1 to n 
+// start: 1
+var x = 1 
+// stop: n
+for (var i=1;x<=n; x++) {
+    console.log(x);
+// step: +1
+};
+// Question 3
+// Write a function for the number of Red Tail Pilots
+// As well as a function for the types of planes
+// As well as a seperate function for the new tricks
+// Plus new US Military Proceedures they invented 
+// Teaching the US Air Fource how to fly 
+// Due to the fact taht the US Air Force originally STEMs from the United States Army
+}
+// Write the function simpleEvenAdding(num)
+//taking a number and add up all the numbers 
+// from 1 to num.
+function simpleEvenAdding(num){
+    // store the answer
+    var solution=...;
+    //loop over numbers from 1 to num
+    for(...;...;...){
+// check if current index is even, odd, or fractional
+        if(...){
+
+        }
+    }
+    return solution;
+}
